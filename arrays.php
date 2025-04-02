04aa3e449b667ce3c19eb3df9985a324919c49c5
ledion <145694386+LedionMakolli@users.noreply.github.com>
2025-04-02 15:37:25 +0200

    Update arrays.php
    Krijimi i vargut multidimensional, asociativ dhe atij numerik

--- arrays.php
+++ arrays.php
@@ -1,3 +1,29 @@
 <?php 
  echo "HELLLOORRR DUA";
+
+ $bahkpunimet = [
+    "Ariana Grande" => [
+        "One Last Time" => [123123123],
+        "One Last Time" => [123123123],
+        "One Last Time" => [123123123]
+    ], "Ed Sheeran" => [
+        "Shape of you" =>[1123123],
+        "Galway girl" =>[1123123],
+        "Photograph" =>[1123123]
+    ], "Majku" => [
+        "Dashni" => [123123]
+    ]
+];
+
+    $rankimet = [
+        "Ariana Grande" => 1,
+        "Ed Sheeran" => 2,
+        "Majku" => 3
+    ];
+
+
+    $udhetimet = ["New York","Berlin","Amsterdan"];
+
+    
+
 ?>
