1225e7def876d8b4c7c1adcda7a56d1e4b2a8e96
duazogu <161737785+duazogu@users.noreply.github.com>
2025-04-02 21:46:03 +0200

    Update arrays.php
    Metodat infoTiketa dhe kalkuloCmiminTotal

--- arrays.php
+++ arrays.php
@@ -1,15 +1,17 @@
 <?php 
- echo "HELLLOORRR DUA";
+ 
 
  $djEmri="Illyrian";
  $tourViti=2025;
  $tiketaCmimi=100;
- $cmimiTotal="";
-var_dump($djEmri);
-var_dump($tiketaCmimi);
- 
 
- $bashkepunimet = [
+ 
+var_dump($djEmri);
+echo"</br>";
+var_dump($tiketaCmimi);
+echo"</br>";
+ 
+$bashkepunimet = [
     "Ariana Grande" => [
         "One Last Time" => [123120023],
         "One Last Time" => [873123123],
@@ -23,8 +25,19 @@ var_dump($tiketaCmimi);
     ]
 ];
 
+function infoTiketa($tiketaCmimi){
+    return "Cmimi i biletes per koncertin eshte $tiketaCmimi euro";
+}
+echo infoTiketa($tiketaCmimi);
+
+$quantity = 2;  // a duhet me qit te tickets si input
 function kalkuloCmiminTotal($tiketaCmimi, $quantity){
-    return  $tiketaCmimi * $quantity;
+    return $tiketaCmimi * $quantity;
+   
+ }
+ $cmimiTotal= kalkuloCmiminTotal($tiketaCmimi, $quantity);
+if($cmimiTotal !== 0){ 
+    echo"<p>Cmimi total per $quantity bileta eshte $cmimiTotal euro</p>";
  }
 
     $rankimet = [
