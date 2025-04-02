470412fd9f2e083b2305e0b4fb255458106f0bca
ledion <145694386+LedionMakolli@users.noreply.github.com>
2025-04-02 15:50:56 +0200

    Update arrays.php
    sortimi i vargut numerik

--- arrays.php
+++ arrays.php
@@ -1,11 +1,13 @@
 <?php 
  echo "HELLLOORRR DUA";
 
+ 
+
  $bahkpunimet = [
     "Ariana Grande" => [
-        "One Last Time" => [123123123],
-        "One Last Time" => [123123123],
-        "One Last Time" => [123123123]
+        "One Last Time" => [123120023],
+        "One Last Time" => [873123123],
+        "One Last Time" => [903123123]
     ], "Ed Sheeran" => [
         "Shape of you" =>[1123123],
         "Galway girl" =>[1123123],
@@ -25,5 +27,6 @@
     $udhetimet = ["New York","Berlin","Amsterdan"];
 
     
-
+    rsort($rankimet); 
+    sort($rankimet);
 ?>
