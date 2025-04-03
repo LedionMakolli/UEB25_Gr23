cf6d858a8409bc90da7371f622bfcfe23f4fecbd
Muhamedjakupi1 <muhamedjakupi056@gmail.com>
2025-04-03 21:45:44 +0200

    Krijimi i klases User - per perdoruesit qe behen logIn ose SignUp

--- login.php
+++ login.php
@@ -163,5 +163,45 @@
             this.reset();
         });
     </script>
+    <?php
+    class User {
+        private $id;
+        protected $email;
+        protected $password;
+        public $created_at;
+        
+        public function __construct($email, $password) {
+            $this->email = $email;
+            $this->setPassword($password); 
+            $this->created_at = date('Y-m-d H:i:s');
+            $this->id = uniqid(); 
+        }
+                public function __destruct() {
+        }
+                public function getId() {
+            return $this->id;
+        }
+        
+        public function getEmail() {
+            return $this->email;
+        }
+        
+        public function setEmail($email) {
+            $this->email = $email;
+        }
+        
+        public function getPassword() {
+            return $this->password;
+        }
+        
+        public function setPassword($password) {
+            $this->password = password_hash($password, PASSWORD_DEFAULT);
+        }
+        
+        public function verifyPassword($inputPassword) {
+            return password_verify($inputPassword, $this->password);
+        }
+    }
+    ?>
 </body>
 </html>
