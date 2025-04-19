1277f66daca8dbfa2bc01728e399d667f5a7e946
ledion <145694386+LedionMakolli@users.noreply.github.com>
2025-04-19 17:16:56 +0200

    Krijmi i php Array per kenget ne SONGS
    Krijimi i nje array per te ruajtur te dhenat e kengeve

--- songs.php
+++ songs.php
@@ -352,8 +352,86 @@
 <div id="songs-one-content">
 <div class="center">
 <div id="songs-container">
+        <?php 
+        //Array PHP per kenget 
+
+        $songsForDisplay = [
+            [
+                'title' => 'Love Galore', 
+                'plays' => 29800, 
+                'artist' => 'Illyric Ft. Sza', 
+                'image' => 'foto/sza.jpg', 
+                'audio' => 'songs/SZA - Love Galore (Lyrics) ft. Travis Scott.mp3',
+                'id' => 'lovegalore'
+            ],
+        [
+            'title' => 'Space Bound', 
+            'plays' => 10200, 
+            'artist' => 'Illyric Ft. Eminem', 
+            'image' => 'foto/eminem.jpeg', 
+            'audio' => 'songs/Eminem - Space Bound (Lyrics).mp3',
+            'id' => 'spacebound'
+        ],
+        [
+            'title' => 'Heartles', 
+            'plays' => 37300, 
+            'artist' => 'Illyric Ft. Kanye West', 
+            'image' => 'foto/kanye.jpeg', 
+            'audio' => 'songs/Kanye West - Heartless (HD).mp3',
+            'id' => 'heartless'
+        ],
+        [
+            'title' => 'H.O.L.L.A', 
+            'plays' => 2000, 
+            'artist' => 'Illyric Ft. Busta Rhymes', 
+            'image' => 'foto/bustarhymes.jpeg', 
+            'audio' => 'songs/Busta Rhymes - H.O.L.L.A.mp3',
+            'id' => 'holla'
+        ],
+        [
+            'title' => 'Starlight Interlude', 
+            'plays' => 21100, 
+            'artist' => 'Illyric Ft. The Weeknd', 
+            'image' => 'foto/weeknd.png', 
+            'audio' => 'songs/The Weeknd & Lana Del Rey - Stargirl Interlude (Lyrics).mp3',
+            'id' => 'stargirlinterlude'
+        ],
+        [
+            'title' => 'One Last Time', 
+            'plays' => 7800, 
+            'artist' => 'Illyric Ft. LP', 
+            'image' => 'foto/lp.jpeg', 
+            'audio' => 'songs/LP - One Last Time (Lyrics).mp3',
+            'id' => 'onelasttime'
+        ],
+        [
+            'title' => 'Mathematics', 
+            'plays' => 963, 
+            'artist' => 'Illyric Ft. Mos Def', 
+            'image' => 'foto/mosdef.jpeg', 
+            'audio' => 'songs/Mos Def - Mathematics.mp3',
+            'id' => 'mathematics'
+        ],
+        [
+            'title' => 'Ms. Jackson', 
+            'plays' => 43500, 
+            'artist' => 'Illyric Ft. Outkast', 
+            'image' => 'foto/outkast.jpeg', 
+            'audio' => 'songs/Outkast - Ms. Jackson (W.Lyrics).mp3',
+            'id' => 'msjackson'
+        ],
+        [
+            'title' => 'Temperature', 
+            'plays' => 9200, 
+            'artist' => 'Illyric Ft. Sean Paul', 
+            'image' => 'foto/seanpaul.jpeg', 
+            'audio' => 'songs/Temperature-Sean Paul.mp3',
+            'id' => 'temperature'
+        ]
+    ];
 
 
+        ?>
 
     <!-- SONG1 -->
 <div class="song fade-up">
