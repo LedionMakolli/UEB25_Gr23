8e05731ab7869796ec3a3d6aa037805619022df7
EjoneAnalumi <ejoneanalumi@gmail.com>
2025-04-19 22:36:09 +0200

    krijimi i klases Artist

---
+++ artist.php
@@ -0,0 +1,13 @@
+<?php
+
+class Artist {
+    protected string $name;
+
+    public function __construct(string $name) {
+        $this->name = $name;
+    }
+
+    public function getName(): string {
+        return $this->name;
+    }
+}
