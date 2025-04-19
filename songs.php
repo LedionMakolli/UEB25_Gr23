f907b1d73507183913e73fbdee5bec38348915f1
EjoneAnalumi <ejoneanalumi@gmail.com>
2025-04-20 01:19:57 +0200

    refaktorizimi i loop-it per shfaqjen e kengeve
    duke perdorur objektet e klases Song

--- songs.php
+++ songs.php
@@ -593,31 +593,32 @@
     }
     ?>
     <?php // Krijo HTML per te gjitha kenget
-        foreach ($songsForDisplay as $song) {
-            $formattedPlays = ($song['plays'] >= 1000) ? 
-                number_format($song['plays']/1000, 1) . 'K' : 
-                $song['plays'];
+        foreach ($songObjects as $song) {
+            $plays = $song->getPlays();
+            $formattedPlays = $plays >= 1000
+                ? number_format($plays/1000,1).'K'
+                : $plays;
             
             echo '
             <div class="song fade-up">
                 <div class="song-img">
-                    <img src="'.$song['image'].'" alt="'.$song['title'].'">
+                    <img src="'.$song['image'].'" alt="'.$song->getTitle().'">
                 </div>
                 <div class="song-details">
                     <div class="song-details-content">
-                        <div class="song-name">'.$song['title'].'</div>
-                        <div class="artist-name">'.$song['artist'].'</div>
+                        <div class="song-name">'.$song->getTitle().'</div>
+                        <div class="artist-name">'.$song->getName().'</div>
                     </div>
                     <div class="music-player">
-                        <div class="play-song mouse">
-                            <img src="foto/play.png" alt="play" data-song="'.$song['id'].'">
-                            <audio data-audio="'.$song['id'].'">
-                                <source src="'.$song['audio'].'" type="audio/mp3">
-                            </audio>
-                        </div>
-                        <div class="download-song mouse">
-                            <a href="'.$song['audio'].'" download="'.$song['title'].' - '.explode(' ', $song['artist'])[0].'">'.$formattedPlays.'<img src="foto/download.png" alt="download"></a>
-                        </div>
+                        <img src="foto/play.png" alt="play" data-song="'.$song->getId().'">
+                        <audio data-audio="'.$song->getId().'">
+                            <source src="'.$song->getAudio().'" type="audio/mp3">
+                        </audio>
+                    </div>
+                    <div class="download-song mouse">
+                        <a href="'.$song->getAudio().'" download="'.$song->getTitle().' - '.explode(' ', $song->getName())[0].'">
+                            ' . $formattedPlays.'<img src="foto/download.png" alt="download">
+                        </a>
                     </div>
                 </div>
             </div>';
